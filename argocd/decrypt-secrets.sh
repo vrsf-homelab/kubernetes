e3a7e144fe5aab642447d9306adb5926c32fe3f5
Paweł Farys <pmg.farys@gmail.com>
2024-05-27 22:47:40 +0200

    Improved some logic for releasing a new ArgoCD image

--- argocd/decrypt-secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f values.secrets.enc.yaml ]; then
-  echo "Detected secrets for Helm values! Decrypting ..."
-  helm secrets decrypt values.secrets.enc.yaml > values.secrets.yaml
-else
-  echo "No secrets for Helm detected, ignoring ..."
-fi
